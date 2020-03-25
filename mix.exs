2dafefa0352d6e5170a8ecf57b1271645156cf6b
Marek Kaput <marek.kaput@swmansion.com>
2020-03-25 09:59:44 +0100

    update test pipeline & include mix.lock

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule BiMap.Mixfile do
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
-      {:stream_data, "~> 0.1", only: :test}
+      {:stream_data, "~> 0.4.0", only: [:dev, :test]}
     ]
   end
 
